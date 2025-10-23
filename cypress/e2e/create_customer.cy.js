04accf4251316e989db1f5880b1a8ab816033900
davidjaa <116652713+davidjaa@users.noreply.github.com>
2025-10-23 10:06:00 +0300

    initial

--- cypress/e2e/create_customer.cy.js
+++ cypress/e2e/create_customer.cy.js
@@ -1,13 +1,13 @@
-describe('Uue kliendi loomine React + Java + DB2 süsteemis', () => {
+describe('Klientide sisselogimine', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
 
-  it('Lisab uue kliendi ja kuvab edukuse teadet', () => {
+  it('Lisab uue kliendi', () => {
     cy.get('input[name="firstName"]').type('Mari')
     cy.get('input[name="lastName"]').type('Kask')
     cy.get('input[name="personalCode"]').type('49002120011')
-    cy.get('input[name="email"]').type('mari.kask@example.com')
+    cy.get('input[name="email"]').type('mari.kask@example.test')
     cy.get('button[type="submit"]').click()
     cy.contains('Klient on edukalt lisatud DB2 andmebaasi!').should('be.visible')
   })
@@ -16,25 +16,25 @@ describe('Uue kliendi loomine React + Java + DB2 süsteemis', () => {
     cy.get('input[name="firstName"]').type('Mari')
     cy.get('input[name="lastName"]').type('Kask')
     cy.get('input[name="personalCode"]').type('49002120011')
-    cy.get('input[name="email"]').type('mari.kask@example.com')
+    cy.get('input[name="email"]').type('mari.kask@example.test')
     cy.get('button[type="submit"]').click()
     cy.contains('Inimene on juba olemas tabelis!').should('be.visible')
   })
 
-  it('Annab vea kui nimi sisaldab keelatud sümboleid', () => {
+  it('Annab vea kui nimi sisaldab numbreid või märke', () => {
     cy.get('input[name="firstName"]').type('Mari123')
     cy.get('input[name="lastName"]').type('Kask!')
     cy.get('input[name="personalCode"]').type('49002120011')
-    cy.get('input[name="email"]').type('mari@fail.test')
+    cy.get('input[name="email"]').type('mari@example.test')
     cy.get('button[type="submit"]').click()
     cy.contains('Nimi võib sisaldada ainult tähti ja sidekriipsu!').should('be.visible')
   })
 
   it('Annab vea kui isikukood on vale pikkusega', () => {
-    cy.get('input[name="firstName"]').type('Karl')
-    cy.get('input[name="lastName"]').type('Erik')
+    cy.get('input[name="firstName"]').type('Mari')
+    cy.get('input[name="lastName"]').type('Kask')
     cy.get('input[name="personalCode"]').type('12345')
-    cy.get('input[name="email"]').type('karl.erik@example.com')
+    cy.get('input[name="email"]').type('mari@example.test')
     cy.get('button[type="submit"]').click()
     cy.contains('Isikukood peab olema 11 numbrit!').should('be.visible')
   })
